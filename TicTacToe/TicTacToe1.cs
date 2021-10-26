b6f77fedc6bca3bfe5d72b6486e3ccbfa099bfb7
1010raghav <89019832+1010raghav@users.noreply.github.com>
2021-10-26 19:28:51 +0530

    TicTacToe Board creation

--- TicTacToe/TicTacToe1.cs
+++ TicTacToe/TicTacToe1.cs
@@ -11,8 +11,9 @@ namespace TicTacToe
         public static void Board()
         {
             for (int i = 1; i < a.Length; i++)
+            {
                 a[i] = '';
-
+            }
             Console.WriteLine(a[i]);
 
         }
