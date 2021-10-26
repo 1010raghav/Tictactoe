2bf81a82beb84aa57308c539dc621695063e840c
Raghav <raghavwandre1234@gmail.com>
2021-10-26 18:51:14 +0530

    creation of Board

---
+++ TicTacToe/TicTacToe1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class TicTacToe1
+    {
+        
+
+        public void UC1()
+        {
+            int[][] tictactoeArray = new int[10][];
+            tictactoeArray[0] = new int[];
+            tictactoeArray[1] = new int[];
+            tictactoeArray[2] = new int[];
+            tictactoeArray[3] = new int[];
+            tictactoeArray[4] = new int[];
+            tictactoeArray[5] = new int[];
+            tictactoeArray[6] = new int[];
+            tictactoeArray[7] = new int[];
+            tictactoeArray[8] = new int[];
+            tictactoeArray[9] = new int[];
+            tictactoeArray[10] = new int[];
+
+        }
+    }
+}
